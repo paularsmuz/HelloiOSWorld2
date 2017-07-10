2c2333f5be60ab246b2bb6ad2a3e41026780a0ae
paularsmuz <paularsmuz@gmail.com>
2017-07-11 02:22:30 +0300

    Финал

--- HelloiOSWorld2/ViewController.swift
+++ HelloiOSWorld2/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+   
+    @IBOutlet weak var greetingLabel: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var nameInput: UITextField!
 
+    @IBOutlet weak var nameLabel: UILabel!
+
+
+    @IBAction func greetingButtonPress(_ sender: Any) {
+        
+        // выполнить в конце метода
+        defer {
+            // скрыть клавиатуру (активное поле отвечает первым)
+            nameInput.resignFirstResponder()
+        }
+        
+    // проверяем есть ли что-либо в поле ввода
+        guard let name = nameInput.text,
+        name.isEmpty == false else {
+            return
+        }
+        
+                
+        nameLabel.text = "Привет,  \(name)!"
+    }
 
 }
 
